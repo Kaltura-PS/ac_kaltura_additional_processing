131d375e4add1cb9e6c87d77c6b833163cadaa5c
hilak <hila.karimov@kaltura.com>
2018-12-18 01:04:24 +0200

    Update get_recording_files.sh

--- get_recording_files.sh
+++ get_recording_files.sh
@@ -3,18 +3,18 @@ if [ $# -lt 1 ]; then
     echo "Usage $0 <recording ID>"
     exit 1
 fi
-TMP=$ID/tmp
+ID="$1"
 if [ -z "$OUTDIR" ]; then
     OUTDIR=/tmp/ac_output
 fi
 mkdir -p $OUTDIR
 cd $OUTDIR
 rm -rf $ID
-mkdir -p $TMP
-
+mkdir -p $ID
 COOKIE=`curl -I "$AC_ENDPOINT/api/xml?action=login&login=$AC_USERNAME&password=$AC_PASSWD" | grep "Set-Cookie:" | awk -F " " '{print $2}'`
 if [ ! -r $ID.zip ]; then
+        echo $ID
     curl -q -b "$COOKIE" "$AC_ENDPOINT/$ID/output/$ID.zip?download=zip" > $ID.zip
 fi
 
-unzip -o -d $TMP $ID.zip
+unzip -o -d $ID $ID.zip
